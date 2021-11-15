3c8875dba2b3735c10cd659415161c8258a80c8d
alina-torr <alina.ysic@gmail.com>
2021-11-15 13:43:25 +0300

    Scripted auto-commit on change (2021-11-15 13:43:25) by gitwatch.sh

--- src/main/java/systems/lab3/FlightApp.java
+++ src/main/java/systems/lab3/FlightApp.java
@@ -5,7 +5,9 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightApp {
-    SparkConf conf = new SparkConf().setAppName("lab3");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-    JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
+    }
 }
