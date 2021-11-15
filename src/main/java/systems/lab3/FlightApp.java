4c2a223d5181a2d694b409ba6eb3c13e5aca1bbe
alina-torr <alina.ysic@gmail.com>
2021-11-15 16:32:28 +0300

    Scripted auto-commit on change (2021-11-15 16:32:28) by gitwatch.sh

--- src/main/java/systems/lab3/FlightApp.java
+++ src/main/java/systems/lab3/FlightApp.java
@@ -1,5 +1,6 @@
 package systems.lab3;
 
+import org.apache.hadoop.io.Text;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -7,12 +8,23 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 public class FlightApp {
+
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_QUOTE = "\"";
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
         JavaPairRDD<Tuple2, FlightSerializable> wordsWithCount = airportFile.mapToPair(
-                s -> {
+                value -> {
+                    String[] flightInfo = value.replace(DELIMITER_QUOTE, "").split(DELIMITER_COMMA);
+
+                    int airportId = Integer.parseInt(flightInfo[CODE_POS]);
+                    String delay = flightInfo[DELAY_POS];
+                    if (!delay.isEmpty() && Float.parseFloat(delay) != 0) {
+                        context.write(new AirportIdWritableComparable(airportId, INDICATOR), new Text(delay));
+                    }
 
 
                     return null;
