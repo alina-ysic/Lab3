e31b238db1f7b6d9b16813dbc214b2974a631c3f
alina-torr <alina.ysic@gmail.com>
2021-11-15 12:32:14 +0300

    Scripted auto-commit on change (2021-11-15 12:32:14) by gitwatch.sh

--- src/main/java/systems/lab3/FlightApp.java
+++ src/main/java/systems/lab3/FlightApp.java
@@ -1,4 +1,9 @@
 package systems.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class FlightApp {
+    SparkConf conf = new SparkConf().setAppName("lab3");
+    JavaSparkContext sc = new JavaSparkContext(conf);
 }
