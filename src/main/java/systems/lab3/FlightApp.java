b490d7f39fa9b043d4956ca984eeef102bd79d8e
alina-torr <alina.ysic@gmail.com>
2021-11-15 14:27:18 +0300

    Scripted auto-commit on change (2021-11-15 14:27:18) by gitwatch.sh

--- src/main/java/systems/lab3/FlightApp.java
+++ src/main/java/systems/lab3/FlightApp.java
@@ -1,13 +1,18 @@
 package systems.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class FlightApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
+                s -> new Tuple2<>(s, 1l)
+        );
     }
 }
