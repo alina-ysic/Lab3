796e61ee0b4599e98433b80d10f70b508443982b
alina-torr <alina.ysic@gmail.com>
2021-11-15 15:39:52 +0300

    Scripted auto-commit on change (2021-11-15 15:39:52) by gitwatch.sh

--- src/main/java/systems/lab3/FlightApp.java
+++ src/main/java/systems/lab3/FlightApp.java
@@ -14,10 +14,10 @@ public class FlightApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
         System.out.println("AAAAA");
-        /*JavaRDD<String> splitted = airportFile.flatMap(
+        JavaRDD<String> splitted = airportFile.flatMap(
                 s -> Arrays.stream(s.split(" ")).iterator()
-        );*/
-        JavaPairRDD<String, Long> wordsWithCount = airportFile.mapToPair(
+        );
+        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
                 s -> {
                     //System.out.println("AAAAA");
                     //System.out.println(s);
